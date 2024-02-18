753a4c83138d84da496ae3613f2d7835d07e24f2
scliaowen <415156969@qq.com>
2024-02-18 23:12:35 +0800

    初始化项目

---
+++ src/main/java/com/mall/Mall4javaApplication.java
@@ -0,0 +1,13 @@
+package com.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall4javaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall4javaApplication.class, args);
+    }
+
+}
